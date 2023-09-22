128968f4460f9a85cb72bdc18489b52d12b6f114
Arthri <41360489+a@users.noreply.github.com>
2023-09-22 16:15:56 +0000

    Add basic project manager

---
+++ src/Belp.Build.Test.MSBuild.XUnit/Resources/ProjectManager.cs
@@ -0,0 +1,64 @@
+﻿#nullable enable
+#if !BELP_BUILD_TEST_MSBUILD_XUNIT_ENABLE_WARNINGS
+#pragma warning disable
+#endif
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Belp.Build.Test.MSBuild.XUnit.Resources;
+
+internal static partial class ProjectManager
+{
+    public static partial class Paths
+    {
+#if !BELP_BUILD_TEST_MSBUILD_XUNIT_RESOURCES_PROJECTS_ROOT_SPECIFIED
+        public static string ProjectsRoot => throw new NotImplementedException();
+#endif
+
+        public static string TempRoot { get; } = Path.Combine(
+            Path.GetTempPath(),
+            "23bf55c5-7020-43d0-a313-9695fe6c313b",
+            "Belp.SDK.Test.MSBuild.XUnit",
+            typeof(ProjectManager).Assembly.GetName().Name ?? throw new InvalidProgramException("Assembly name is null.")
+        );
+
+        public static string ProjectCache { get; } = Path.Combine(TempRoot, "projects");
+    }
+
+    public static void ClearProjectCache()
+    {
+        Directory.Delete(Paths.ProjectCache, true);
+        _ = Directory.CreateDirectory(Paths.ProjectCache);
+    }
+
+    public static string CloneProject(string projectName, [CallerMemberName] string? callerMemberName = null)
+    {
+        if (callerMemberName is null)
+        {
+            throw new ArgumentNullException(nameof(callerMemberName));
+        }
+
+        string sourceDirectory = Path.Combine(Paths.ProjectsRoot, projectName);
+        string destinationDirectory = Path.Combine(Paths.ProjectCache, projectName, callerMemberName);
+
+        var queue = new Queue<(string, string)>();
+        queue.Enqueue((sourceDirectory, destinationDirectory));
+        while (queue.Count > 0 && queue.Dequeue() is var (originDir, targetDir))
+        {
+            foreach (string file in Directory.GetFiles(originDir))
+            {
+                File.Copy(file, Path.Combine(targetDir, Path.GetFileName(file)));
+            }
+
+            foreach (string dir in Directory.GetDirectories(originDir))
+            {
+                queue.Enqueue((dir, Path.Combine(targetDir, Path.GetDirectoryName(dir) ?? "")));
+            }
+        }
+
+        return destinationDirectory;
+    }
+}
