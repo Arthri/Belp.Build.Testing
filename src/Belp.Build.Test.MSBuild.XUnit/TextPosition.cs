3590369fe2e39ee09073ec39f8bdf4e8364e5aed
Arthri <41360489+a@users.noreply.github.com>
2023-09-22 15:27:54 +0000

    Merge TextPosition into TextSpan

--- src/Belp.Build.Test.MSBuild.XUnit/TextPosition.cs
+++
@@ -1,8 +0,0 @@
-﻿#nullable enable
-#if !BELP_BUILD_TEST_MSBUILD_XUNIT_ENABLE_WARNINGS
-#pragma warning disable
-#endif
-
-namespace Belp.Build.Test.MSBuild.XUnit;
-
-internal record struct TextPosition(int Line, int Column);
--- src/Belp.Build.Test.MSBuild.XUnit/TextSpan.cs
+++ src/Belp.Build.Test.MSBuild.XUnit/TextSpan.cs
@@ -7,8 +7,10 @@ using Microsoft.Build.Framework;
 
 namespace Belp.Build.Test.MSBuild.XUnit;
 
-internal record struct TextSpan(TextPosition Start, TextPosition End)
+internal record struct TextSpan(TextSpan.Position Start, TextSpan.Position End)
 {
+    public record struct Position(int Line, int Column);
+
     public TextSpan(BuildMessageEventArgs e)
         : this(new(e.LineNumber, e.ColumnNumber), new(e.EndLineNumber, e.EndColumnNumber))
     {
