8cce62788cffa9b084bf2d9cc918e7f03856753a
Arthri <41360489+a@users.noreply.github.com>
2023-11-23 17:12:34 +0000

    Add xml documentation to module initializer

--- src/Belp.Build.Test.MSBuild.XUnit/Initializer.cs
+++ src/Belp.Build.Test.MSBuild.XUnit/Initializer.cs
@@ -1,14 +1,21 @@
-using Microsoft.Build.Locator;
+﻿using Microsoft.Build.Locator;
 using System;
 using System.Linq;
 using System.Runtime.CompilerServices;
 
+/// <summary>
+/// Contains the module initialization.
+/// </summary>
 file static class Initializer
 {
+    /// <summary>
+    /// Invokes <see cref="MSBuildLocator"/> to add/link the MSBuild assemblies to the runtime.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">A suitable version of .NET SDK was not found.</exception>
 #pragma warning disable CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
     [ModuleInitializer]
 #pragma warning restore CA2255 // The 'ModuleInitializer' attribute should not be used in libraries
-    public static void InitializeModule()
+    internal static void InitializeModule()
     {
         if (MSBuildLocator.IsRegistered)
         {
