b1231cb075c2c29ec0b36e884fa4245a94199d61
Arthri <41360489+a@users.noreply.github.com>
2024-03-31 03:30:05 +0000

    Logger refactor

---
+++ src/Belp.Build.Test.MSBuild/Loggers/DiagnosticEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using Belp.Build.Test.MSBuild.ObjectModel;
+
+namespace Belp.Build.Test.MSBuild.Loggers;
+
+/// <summary>
+/// Represents a method that will handle a diagnostic event.
+/// </summary>
+/// <param name="diagnostic"></param>
+public delegate void DiagnosticEventHandler(Diagnostic diagnostic);
---
+++ src/Belp.Build.Test.MSBuild/Loggers/XUnitLogger.cs
@@ -0,0 +1,171 @@
+﻿using Belp.Build.Test.MSBuild.ObjectModel;
+using Microsoft.Build.Framework;
+using Xunit.Abstractions;
+
+namespace Belp.Build.Test.MSBuild.Loggers;
+
+/// <summary>
+/// Provides an adapter for <see cref="ITestOutputHelper"/> to <see cref="ILogger"/>.
+/// </summary>
+public class XUnitLogger : ITestOutputHelper, ILogger
+{
+    private readonly MSBuildDiagnosticLogger _logger;
+    private readonly ITestOutputHelper _output;
+    private IEventSource _eventSource = null!;
+
+    string? ILogger.Parameters
+    {
+        get => null;
+
+        set
+        {
+        }
+    }
+
+    /// <inheritdoc />
+    public LoggerVerbosity Verbosity { get; set; } = LoggerVerbosity.Normal;
+
+
+
+    /// <inheritdoc cref="MSBuildDiagnosticLogger.OnError" />
+    public event DiagnosticEventHandler? OnError
+    {
+        add => _logger.OnError += value;
+        remove => _logger.OnError -= value;
+    }
+
+    /// <inheritdoc cref="MSBuildDiagnosticLogger.OnWarning" />
+    public event DiagnosticEventHandler? OnWarning
+    {
+        add => _logger.OnWarning += value;
+        remove => _logger.OnWarning -= value;
+    }
+
+    /// <inheritdoc cref="MSBuildDiagnosticLogger.OnMessage" />
+    public event DiagnosticEventHandler? OnMessage
+    {
+        add => _logger.OnMessage += value;
+        remove => _logger.OnMessage -= value;
+    }
+
+    /// <inheritdoc cref="MSBuildDiagnosticLogger.OnDiagnostic" />
+    public event DiagnosticEventHandler? OnDiagnostic
+    {
+        add => _logger.OnDiagnostic += value;
+        remove => _logger.OnDiagnostic -= value;
+    }
+
+
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="XUnitLogger"/> for the specified <paramref name="output"/>.
+    /// </summary>
+    /// <param name="output">The <see cref="ITestOutputHelper"/> to adapt.</param>
+    public XUnitLogger(ITestOutputHelper output)
+    {
+        _logger = new MSBuildDiagnosticLogger();
+        _output = output;
+    }
+
+    #region ILogger
+
+    /// <inheritdoc />
+    public virtual void Initialize(IEventSource eventSource)
+    {
+        _eventSource = eventSource;
+
+        eventSource.BuildFinished += OnBuildStatus;
+        eventSource.BuildStarted += OnBuildStatus;
+        eventSource.ProjectFinished += OnBuildStatus;
+        eventSource.ProjectStarted += OnBuildStatus;
+
+        OnError += OnErrorRaised;
+        OnWarning += OnWarningRaised;
+        OnMessage += OnMessageRaised;
+    }
+
+    /// <inheritdoc />
+    public virtual void Shutdown()
+    {
+        _eventSource.BuildFinished -= OnBuildStatus;
+        _eventSource.BuildStarted -= OnBuildStatus;
+        _eventSource.ProjectFinished -= OnBuildStatus;
+        _eventSource.ProjectStarted -= OnBuildStatus;
+
+        OnError -= OnErrorRaised;
+        OnWarning -= OnWarningRaised;
+        OnMessage -= OnMessageRaised;
+    }
+
+    /// <summary>
+    /// Runs when any status update is raised.
+    /// </summary>
+    /// <param name="sender">The status update's sender.</param>
+    /// <param name="e">The raised status update.</param>
+    protected virtual void OnBuildStatus(object sender, BuildStatusEventArgs e)
+    {
+        if (e.Message is not null)
+        {
+            WriteLine(e.Message);
+        }
+    }
+
+    /// <summary>
+    /// Runs when an error is raised.
+    /// </summary>
+    /// <param name="diagnostic">The diagnostic that was captured.</param>
+    protected virtual void OnErrorRaised(Diagnostic diagnostic)
+    {
+        if (Verbosity >= LoggerVerbosity.Quiet)
+        {
+            WriteLine(diagnostic.ToString());
+        }
+    }
+
+    /// <summary>
+    /// Runs when a warning is raised.
+    /// </summary>
+    /// <param name="diagnostic">The diagnostic that was captured.</param>
+    protected virtual void OnWarningRaised(Diagnostic diagnostic)
+    {
+        if (Verbosity >= LoggerVerbosity.Minimal)
+        {
+            WriteLine(diagnostic.ToString());
+        }
+    }
+
+    /// <summary>
+    /// Runs when a message is raised.
+    /// </summary>
+    /// <param name="diagnostic">The diagnostic that was captured.</param>
+    /// <exception cref="NotSupportedException">The message has an unsupported importance level.</exception>
+    protected virtual void OnMessageRaised(Diagnostic diagnostic)
+    {
+        if (
+            Verbosity switch
+            {
+                LoggerVerbosity.Diagnostic => diagnostic.Severity <= Diagnostic.SeverityLevel.Diagnostic,
+                LoggerVerbosity.Detailed => diagnostic.Severity <= Diagnostic.SeverityLevel.Verbose,
+                LoggerVerbosity.Quiet or LoggerVerbosity.Minimal or LoggerVerbosity.Normal => diagnostic.Severity <= Diagnostic.SeverityLevel.Informational,
+                _ => throw new NotSupportedException(),
+            }
+        )
+        {
+            WriteLine(diagnostic.ToString());
+        }
+    }
+
+    #endregion
+
+    /// <inheritdoc />
+    public void WriteLine(string message)
+    {
+        _output.WriteLine(message);
+    }
+
+    /// <inheritdoc />
+    public void WriteLine(string format, params object[] args)
+    {
+        _output.WriteLine(format, args);
+    }
+}
