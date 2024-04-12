b08910de21e0228ad310f6c5a10b4d7745bd8137
Arthri <41360489+a@users.noreply.github.com>
2024-04-12 09:00:08 +0000

    Add conversion from tuple to TextSpan and Position

--- src/Belp.Build.Test.MSBuild/TextSpan.cs
+++ src/Belp.Build.Test.MSBuild/TextSpan.cs
@@ -12,11 +12,30 @@ public record struct TextSpan(TextSpan.Position Start, TextSpan.Position End)
     /// </summary>
     /// <param name="Line">The line at which the position begins.</param>
     /// <param name="Column">The column of the specified <paramref name="Line"/> at which the position begins.</param>
-    public record struct Position(int Line, int Column);
+    public record struct Position(int Line, int Column)
+    {
+        /// <summary>
+        /// Defines and implicit conversion from an <see cref="int"/>-<see cref="int"/> tuple to a <see cref="Position"/>.
+        /// </summary>
+        /// <param name="tuple">The tuple to convert to a <see cref="Position"/>.</param>
+        public static implicit operator Position((int Line, int Column) tuple)
+        {
+            return new(tuple.Line, tuple.Column);
+        }
+    }
 
     /// <inheritdoc />
     public override readonly string ToString()
     {
         return $"{Start.Line}:{Start.Column},{End.Line}:{End.Column}";
+    }
+
+    /// <summary>
+    /// Defines and implicit conversion from a <see cref="Position"/>-<see cref="Position"/> tuple to a <see cref="TextSpan"/>.
+    /// </summary>
+    /// <param name="tuple">The tuple to convert to a <see cref="TextSpan"/>.</param>
+    public static implicit operator TextSpan((Position Start, Position End) tuple)
+    {
+        return new(tuple.Start, tuple.End);
     }
 }
