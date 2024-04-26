713245d112fb598057502c1a3a607a67a6e036a7
Arthri <41360489+a@users.noreply.github.com>
2024-04-26 17:37:24 +0000

    Add path helpers to TestPackage

--- src/Belp.Build.Test.MSBuild/Resources/TestPackage.cs
+++ src/Belp.Build.Test.MSBuild/Resources/TestPackage.cs
@@ -33,4 +33,49 @@ public readonly struct TestPackage
         Version = packageVersion;
         Path = IOPath.Combine(TestPaths.PackagesCache, id.ToLower(), packageVersion);
     }
+
+    /// <summary>
+    /// Gets the path to a file in the package.
+    /// </summary>
+    /// <returns>The path to a file in the package.</returns>
+    public string GetPath(string path1)
+    {
+        return IOPath.Combine(Path, path1);
+    }
+
+    /// <summary>
+    /// Gets the path to a file in the package.
+    /// </summary>
+    /// <param name="path1">The first path to combine.</param>
+    /// <param name="path2">The second path to combine.</param>
+    /// <returns>The path to a file in the package.</returns>
+    public string GetPath(string path1, string path2)
+    {
+        return IOPath.Combine(Path, path1, path2);
+    }
+
+    /// <summary>
+    /// Gets the path to a file in the package.
+    /// </summary>
+    /// <param name="path1">The first path to combine.</param>
+    /// <param name="path2">The second path to combine.</param>
+    /// <param name="path3">The third path to combine.</param>
+    /// <returns>The path to a file in the package.</returns>
+    public string GetPath(string path1, string path2, string path3)
+    {
+        return IOPath.Combine(Path, path1, path2, path3);
+    }
+
+    /// <summary>
+    /// Gets the path to a file in the package.
+    /// </summary>
+    /// <param name="paths">An array of paths to combine.</param>
+    /// <returns>The path to a file in the package.</returns>
+    public string GetPath(params string[] paths)
+    {
+        string[] pathsToCombine = new string[paths.Length + 1];
+        paths.CopyTo(pathsToCombine.AsSpan(1));
+        pathsToCombine[0] = Path;
+        return IOPath.Combine(paths);
+    }
 }
