c4544582d72abae9bdb23f3029f26b4ccb4a58b7
Arthri <41360489+a@users.noreply.github.com>
2024-05-16 17:02:44 +0000

    Allow users to create MSBuildItems

--- src/Belp.Build.Test.MSBuild/MSBuildItem.cs
+++ src/Belp.Build.Test.MSBuild/MSBuildItem.cs
@@ -1,4 +1,5 @@
-using Microsoft.Build.Execution;
+﻿using Microsoft.Build.Execution;
+using System.Collections.Frozen;
 
 namespace Belp.Build.Test.MSBuild;
 
@@ -30,34 +31,38 @@ public readonly struct MSBuildItem
         }
     }
 
-    private readonly ProjectItemInstance _item;
-
     /// <summary>
     /// Gets the item's type.
     /// </summary>
-    public string Type => _item.ItemType;
+    public string Type { get; }
 
     /// <summary>
     /// Gets the item's identity or the evaluated contents of its Include attribute.
     /// </summary>
-    public string Identity => _item.EvaluatedInclude;
+    public string Identity { get; }
 
     /// <summary>
-    /// Gets the item's metadata.
+    /// Gets the item's metadata in dictionary form. Dictionary keys represent metadata names, while values associated with those keys are the values associated with the metadata.
     /// </summary>
-    public IEnumerable<ItemMetadata> Metadata
+    public IReadOnlyDictionary<string, string> Metadata { get; }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="MSBuildItem"/> for the specified <paramref name="type"/> and <paramref name="identity"/> with the specified <paramref name="metadata"/>.
+    /// </summary>
+    /// <param name="type">The item's type.</param>
+    /// <param name="identity">The item's identity or the contents of the Include attribute</param>
+    /// <param name="metadata">The item's metadata in dictionary form. Dictionary keys represent metadata names, while values associated with those keys are the values associated with the metadata. Defaults to <see langword="null" /> or no metadata.</param>
+    public MSBuildItem(string type, string identity, IReadOnlyDictionary<string, string>? metadata = null)
     {
-        get
-        {
-            foreach (ProjectMetadataInstance? metadata in _item.Metadata)
-            {
-                yield return new ItemMetadata(metadata);
-            }
-        }
+        Type = type;
+        Identity = identity;
+        Metadata = metadata ?? FrozenDictionary<string, string>.Empty;
     }
 
     internal MSBuildItem(ProjectItemInstance item)
     {
-        _item = item;
+        Type = item.ItemType;
+        Identity = item.EvaluatedInclude;
+        Metadata = item.Metadata.ToDictionary(m => m.Name, m => m.EvaluatedValue).AsReadOnly();
     }
 }
